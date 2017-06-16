62b586b3561f6399a0fa832028041d206d5d31b1
Joe Huss <detain@interserver.net>
2017-06-16 08:14:32 -0400

    updates to the plugin migrating the varios data in the myadmin.php file into the Plugin class itself so i can then phase out the myadmin.php file and then a developer only needs to setup this one simple file to setup a new plugin.

--- src/Plugin.php
+++ src/Plugin.php
@@ -7,7 +7,20 @@ use Symfony\Component\EventDispatcher\GenericEvent;
 
 class Plugin {
 
+	public static $name = 'Openvz Vps';
+	public static $description = 'Allows selling of Openvz Server and VPS License Types.  More info at https://www.netenberg.com/openvz.php';
+	public static $help = 'It provides more than one million end users the ability to quickly install dozens of the leading open source content management systems into their web space.  	Must have a pre-existing cPanel license with cPanelDirect to purchase a openvz license. Allow 10 minutes for activation.';
+	public static $module = 'vps';
+	public static $type = 'service';
+
+
 	public function __construct() {
+	}
+
+	public static function Hooks() {
+		return [
+			'vps.settings' => ['Detain\MyAdminOpenvz\Plugin', 'Settings'],
+		];
 	}
 
 	public static function Activate(GenericEvent $event) {
