6530afaa375cf50b5a8650976c2d8a4ae87a5b30
Joe Huss <detain@interserver.net>
2017-07-10 02:51:15 -0400

    fixes switching to using the service type instead of category and some code tweaks

--- src/Plugin.php
+++ src/Plugin.php
@@ -20,22 +20,21 @@ class Plugin {
 	public static function getHooks() {
 		return [
 			self::$module.'.settings' => [__CLASS__, 'getSettings'],
+			//self::$module.'.activate' => [__CLASS__, 'getActivate'],
 			self::$module.'.deactivate' => [__CLASS__, 'getDeactivate'],
 		];
 	}
 
 	public static function getActivate(GenericEvent $event) {
 		$serviceClass = $event->getSubject();
-		if ($event['category'] == SERVICE_TYPES_OPENVZ) {
+		if (in_array($event['type'], [SERVICE_TYPES_OPENVZ, SERVICE_TYPES_SSD_OPENVZ])) {
 			myadmin_log(self::$module, 'info', self::$name.' Activation', __LINE__, __FILE__);
-			function_requirements('activate_openvz');
-			activate_openvz($serviceClass->getIp(), $event['field1']);
 			$event->stopPropagation();
 		}
 	}
 
 	public static function getDeactivate(GenericEvent $event) {
-		if ($event['category'] == SERVICE_TYPES_OPENVZ) {
+		if (in_array($event['type'], [SERVICE_TYPES_OPENVZ, SERVICE_TYPES_SSD_OPENVZ])) {
 			myadmin_log(self::$module, 'info', self::$name.' Deactivation', __LINE__, __FILE__);
 			$serviceClass = $event->getSubject();
 			$GLOBALS['tf']->history->add(self::$module.'queue', $serviceClass->getId(), 'delete', '', $serviceClass->getCustid());
@@ -43,7 +42,7 @@ class Plugin {
 	}
 
 	public static function getChangeIp(GenericEvent $event) {
-		if ($event['category'] == SERVICE_TYPES_OPENVZ) {
+		if (in_array($event['type'], [SERVICE_TYPES_OPENVZ, SERVICE_TYPES_SSD_OPENVZ])) {
 			$serviceClass = $event->getSubject();
 			$settings = get_module_settings(self::$module);
 			$openvz = new Openvz(FANTASTICO_USERNAME, FANTASTICO_PASSWORD);
