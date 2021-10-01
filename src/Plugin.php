0be5a82f5ee9705c7b32f9181642d8d5b0ef1c71
Joe Huss <detain@interserver.net>
2021-10-01 12:20:09 -0400

    updates to the log messages generated by the vps queues adding the command output to the line

--- src/Plugin.php
+++ src/Plugin.php
@@ -108,14 +108,15 @@ class Plugin
 		if (in_array($event['type'], [get_service_define('OPENVZ'), get_service_define('SSD_OPENVZ')])) {
 			$serviceInfo = $event->getSubject();
 			$settings = get_module_settings(self::$module);
-			myadmin_log(self::$module, 'info', self::$name.' Queue '.ucwords(str_replace('_', ' ', $serviceInfo['action'])).' for VPS '.$serviceInfo['vps_hostname'].'(#'.$serviceInfo['vps_id'].'/'.$serviceInfo['vps_vzid'].')', __LINE__, __FILE__, self::$module, $serviceInfo[$settings['PREFIX'].'_id']);
 			$server_info = $serviceInfo['server_info'];
 			if (!file_exists(__DIR__.'/../templates/'.$serviceInfo['action'].'.sh.tpl')) {
 				myadmin_log(self::$module, 'error', 'Call '.$serviceInfo['action'].' for VPS '.$serviceInfo['vps_hostname'].'(#'.$serviceInfo['vps_id'].'/'.$serviceInfo['vps_vzid'].') Does not Exist for '.self::$name, __LINE__, __FILE__, self::$module, $serviceInfo[$settings['PREFIX'].'_id']);
 			} else {
 				$smarty = new \TFSmarty();
 				$smarty->assign($serviceInfo);
-				$event['output'] = $event['output'].$smarty->fetch(__DIR__.'/../templates/'.$serviceInfo['action'].'.sh.tpl');
+				$output = $smarty->fetch(__DIR__.'/../templates/'.$serviceInfo['action'].'.sh.tpl');
+				myadmin_log(self::$module, 'info', self::$name.' Queue '.ucwords(str_replace('_', ' ', $serviceInfo['action'])).' for '.strtoupper($settings['PREFIX']).' '.$serviceInfo['vps_hostname'].'(#'.$serviceInfo['vps_id'].'/'.$serviceInfo['vps_vzid'].'): '.$output, __LINE__, __FILE__, self::$module, $serviceInfo['vps_id']);
+				$event['output'] = $event['output'].$output;
 			}
 			$event->stopPropagation();
 		}
