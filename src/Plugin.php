a509a7b02e90016ef0d1bfb68208762694470c16
Joe Huss <detain@interserver.net>
2022-11-10 02:35:22 -0500

    fixing php notices for when settings are undefined

--- src/Plugin.php
+++ src/Plugin.php
@@ -88,8 +88,8 @@ class Plugin
         $settings->add_text_setting(self::$module, _('Slice OpenVZ Amounts'), 'vps_slice_openvz_shmpages', 'shmpages', _('The total size of shared memory (IPC), shared anonymous mappings and tmpfs objects.'), $settings->get_setting('VPS_SLICE_OPENVZ_SHMPAGES'));
         $settings->add_text_setting(self::$module, _('Slice OpenVZ Amounts'), 'vps_slice_openvz_numiptent', 'numiptent', _('The number of IP packet filtering entries.'), $settings->get_setting('VPS_SLICE_OPENVZ_NUMIPTENT'));
         $settings->setTarget('module');
-        $settings->add_select_master(_(self::$module), _('Default Servers'), self::$module, 'new_vps_openvz_server', _('OpenVZ NJ Server'), NEW_VPS_OPENVZ_SERVER, 6, 1);
-        $settings->add_select_master(_(self::$module), _('Default Servers'), self::$module, 'new_vps_ssd_openvz_server', _('SSD OpenVZ NJ Server'), NEW_VPS_SSD_OPENVZ_SERVER, 5, 1);
+        $settings->add_select_master(_(self::$module), _('Default Servers'), self::$module, 'new_vps_openvz_server', _('OpenVZ NJ Server'), defined('NEW_VPS_OPENVZ_SERVER') ? NEW_VPS_OPENVZ_SERVER : '', 6, 1);
+        $settings->add_select_master(_(self::$module), _('Default Servers'), self::$module, 'new_vps_ssd_openvz_server', _('SSD OpenVZ NJ Server'), defined('NEW_VPS_SSD_OPENVZ_SERVER') ? NEW_VPS_SSD_OPENVZ_SERVER : '', 5, 1);
         //$settings->add_select_master(_(self::$module), _('Default Servers'), self::$module, 'new_vps_la_openvz_server', _('OpenVZ LA Server'), NEW_VPS_LA_OPENVZ_SERVER, 6, 2);
         //$settings->add_select_master(_(self::$module), _('Default Servers'), self::$module, 'new_vps_ny_openvz_server', _('OpenVZ NY4 Server'), NEW_VPS_NY_OPENVZ_SERVER, 0, 3);
         $settings->add_dropdown_setting(self::$module, _('Out of Stock'), 'outofstock_openvz', _('Out Of Stock OpenVZ Secaucus'), _('Enable/Disable Sales Of This Type'), $settings->get_setting('OUTOFSTOCK_OPENVZ'), ['0', '1'], ['No', 'Yes']);
